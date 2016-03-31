26d26f1a80bd5f1bfaf658666293c4f71277674a
Jongwook Chae <jwchae8@gmail.com>
2016-03-31 11:41:30 -0700

    [PA1]Parsing of action, protocol, ip, port, direction

--- pa1/main.c
+++ pa1/main.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdint.h>
+#include <ctype.h>
+#include <string.h>
 #include <pcap/pcap.h>
+
+
+#define PORT_MAX 65535
+#define PORT_MIN 0
+
+
+int is_ip(char* ipstr, uint32_t* ipnum) {
+    int i, dot_count = 0;
+    uint32_t ip_a, ip_b, ip_c, ip_d;
+    if(strcmp(ipstr, "any")) {
+        for(i=0; i<strlen(ipstr); i++) {
+            if(ipstr[i] == '.') {
+                dot_count++;
+            }
+            else if(!isdigit(ipstr[i])) {
+                return -1;
+            }
+        }
+        if(dot_count != 3) {
+            return -1;
+        }
+        sscanf(ipstr, "%d.%d.%d.%d", &ip_a, &ip_b, &ip_c, &ip_d);
+        if(ip_a > 255 || ip_b > 255 || ip_c > 255 || ip_d > 255) {
+            return -1;
+        }
+        *ipnum = ip_a << 24 + ip_b << 16 + ip_c << 8 + ip_d;
+    }
+    else {
+        *ipnum = 0;
+    }
+    return 0;
+}
+
+int is_port(char* portstr, uint16_t* portnum) {
+    int i, tmp;
+    if(strcmp(portstr, "any")) {
+        for(i=0; i<strlen(portstr); i++) {
+            if(!isdigit(portstr[i])) {
+                return -1;
+            }
+        }
+        tmp = atoi(portstr);
+        if(tmp > PORT_MAX) {
+            return -1;
+        }
+        *portnum = (uint16_t) tmp;
+    }
+    else {
+        *portnum = 0;
+    }
+    return 0;
+}
+
 
 struct rule {
     /* ip header */
@@ -31,14 +86,17 @@ struct list_elem {
 
 int main(int argc, char **argv)
 {
-    int i, opt;
+    int i, opt, ret;
     char *interface, *rule_file;
     char *errbuf;
     pcap_t *handle;
     uint8_t *packet;
     struct pcap_pkthdr header;
     FILE *fp;
-    char *rule_token;
+    char rule_token[10], iptoken[4][5];
+    struct rule* new_rule;
+    struct list_elem* rule_list;
+    int pattern_rule_exists;
 
     while((opt = getopt(argc, argv, "i:r:")) != -1) {
         switch(opt) {
@@ -72,11 +130,61 @@ int main(int argc, char **argv)
         fprintf(stderr, "[Error]File open error(file may not exist\n");
         exit(-1);
     }
-
+    pattern_rule_exists = 1;
     while((fscanf(fp, "%s", rule_token)) != EOF) {
-        
+        new_rule = (struct rule*) malloc(sizeof(struct rule));
+        if(pattern_rule_exists) {
+            if(strcmp(rule_token, "alert")) {
+                fprintf(stderr, "[Error]Undefined action : action of rule must be \'alert\'\nor this is one of unread tokens of wrong rule: given token = %s\n", rule_token);
+                continue;
+            }
+            fscanf(fp, "%s", rule_token);
+        }
+        if(strcmp(rule_token, "tcp")) {
+            fprintf(stderr, "[Error]Undefined protocol : protocol of rule must be \'tcp\': given token = %s\n", rule_token);
+            continue;
+        }
+        fscanf(fp, "%s", rule_token);
+        if((ret = is_ip(rule_token, &(new_rule->srcip))) < 0) {
+            fprintf(stderr, "[Error]Not valid ip number : given token = %s\n", rule_token);
+            continue;
+        }
+        fscanf(fp, "%s", rule_token);
+        if((ret = is_port(rule_token, &(new_rule->srcport))) < 0) {
+            fprintf(stderr, "[Error]Not valid port number : given token = %s\n", rule_token);
+            continue;
+        }
+        fscanf(fp, "%s", rule_token);
+        if(strcmp(rule_token, "->")) {
+            fprintf(stderr, "[Error]Not correct format : this rule lacks directional mark: given token = %s\n", rule_token);
+        }
+        fscanf(fp, "%s", rule_token);
+        if((ret = is_ip(rule_token, &(new_rule->destip))) < 0) {
+            fprintf(stderr, "[Error]Not valid ip number : given token = %s\n", rule_token);
+            continue;
+        }
+        fscanf(fp, "%s", rule_token);
+        if((ret = is_port(rule_token, &(new_rule->destport))) < 0) {
+            fprintf(stderr, "[Error]Not valid port number : given token = %s\n", rule_token);
+            continue;
+        }
+        fscanf(fp, "%s", rule_token);
+        if(!strcmp(rule_token, "alert")) {
+            pattern_rule_exists = 0;
+            continue;
+        }
+        pattern_rule_exists = 1;
+        while(true){
+            if(rule_token[0] == '(') {
+                rule_token++;
+            }
+            if(strstr(rule_token, ";)")) {
+                break;
+            }
+            fscanf(fp, "%s", rule_token);
+        }
     }
-
+    fclose(fp);
     while(true) {
         packet = pcap_next(handle, &header);
         
